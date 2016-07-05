2faac36bafee58e8febec9b36efb415b7a2c26ca
Harish.K <harish2704@gmail.com>
2016-07-05 08:38:28 +0530

    update accordingto API

--- run.js
+++ run.js
@@ -7,17 +7,29 @@ var ejs = require('ejs');
 var ctrlFiles = [].slice.call(process.argv, 2 );
 
 
+var methodAliases = {
+  list: 'get',
+  create: 'post',
+  update: 'put',
+};
+[ 'get', 'post', 'put', 'delete', 'patch'].forEach(function(v){ methodAliases[v] = v; });
+
+/* /(list|create|update|get|post|put|delete|patch)(.*)/ */
+var actionNameRegex =  new RegExp('(' + Object.keys( methodAliases ).join('|') + ')(.*)' );
+
+	
+
 function getMethods( ctrlPath ){
   var ctrl = require( ctrlPath );
   return Object.keys( ctrl );
 }
 
 function parseFnName( actionName, controllerName ){
-  var action = actionName.match( /(get|post|put|delete|patch)(.*)/ );
+  var action = actionName.match( actionNameRegex );
   var method = action[1];
   var urlPath = '/' + controllerName +'/'+ action[2].toLowerCase();
   return {
-    method: method,
+    method: methodAliases[method],
     url: urlPath,
     name: actionName,
   }
